2fbe622944c7583a9ecb3d9ef26e07e44fdcc623
Aleksei Gridin <48714129+alexnvb@users.noreply.github.com>
2020-08-24 18:18:09 +0700

    add view by category

--- dartblog/siteblog/blog/models.py
+++ dartblog/siteblog/blog/models.py
@@ -9,6 +9,9 @@ class Category(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255, verbose_name='url', unique=True)
 
+    def get_absolute_url(self):
+        return reverse('category', kwargs={"slug": self.slug})  
+
     def __str__(self):
         return self.title
 
--- dartblog/siteblog/blog/urls.py
+++ dartblog/siteblog/blog/urls.py
@@ -19,6 +19,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', Home.as_view(), name='home'),
-    path('category/<str:slug>/', get_category, name='category'),
+    path('category/<str:slug>/', PostsByCategory.as_view(), name='category'),
     path('post/<str:slug>/', get_post, name='post'),
 ]

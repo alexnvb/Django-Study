fc9bbbe8b5a3299b3332665de89eb1967dc77622
Aleksei Gridin <48714129+alexnvb@users.noreply.github.com>
2020-08-27 18:09:40 +0700

    add search

--- dartblog/siteblog/blog/views.py
+++ dartblog/siteblog/blog/views.py
@@ -11,7 +11,7 @@ class Home(ListView):
     model = Post
     template_name = 'blog/index.html'
     context_object_name = 'posts'
-    paginate_by = 4
+    paginate_by = 3
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -47,7 +47,7 @@ class GetPost(DetailView):
 class PostsByTag(ListView):
     template_name = 'blog/index.html'
     context_object_name = 'posts'
-    paginate_by = 1
+    paginate_by = 3
     allow_empty =False
 
     def get_queryset(self):
@@ -56,6 +56,20 @@ class PostsByTag(ListView):
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Записи по тегу: ' + str(Tag.objects.get(slug=self.kwargs['slug']))
+        return context
+
+
+class Search(ListView):
+    template_name = 'blog/search.html'
+    context_object_name = 'posts'
+    paginate_by = 4
+
+    def get_queryset(self):
+        return Post.objects.filter(title__icontains=self.request.GET.get('s'))
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['s'] = f"s={self.request.GET.get('s')}&"
         return context
 
 
